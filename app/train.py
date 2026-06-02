341ede32f884aed34f4869b3911ff4c913c2bba5
tonyddg <799052200@qq.com>
2026-06-02 20:07:16 +0800

    chore: 更新训练脚本

--- app/train.py
+++ app/train.py
@@ -7,15 +7,22 @@ from vla_bc_policy.train import get_trainer
 
 if __name__ == "__main__":
 
-    torch.set_float32_matmul_precision('medium')
+    torch.set_float32_matmul_precision('high')
 
     dm = Pi0LeRobotDataModule(
-        "trajectories_tidy_house_all_bc_fix_la_state_rot_6d_action_axis_angle",
-        camera_info_list_to_dict_list(FetchStandardCameraInfos),
+        repo_id = "trajectories_tidy_house_all_bc_with_qvel_state_rot_6d_action_axis_angle_train",
+        val_repo_id = "trajectories_tidy_house_all_bc_with_qvel_state_rot_6d_action_axis_angle_val",
+
+        camera_info_list = camera_info_list_to_dict_list(FetchStandardCameraInfos),
         debug_config = False,
 
         batch_size = 1024,
         num_workers = 32,
+
+        vec_obs_keys = ["pi0_actions_ref", "pi0_state", "qpos", "qvel"],
+        vec_obs_compress_key = {
+            "qvel": 1.5 
+        },
     )
     pm = PolicyModule(
         "post_mix",
@@ -28,7 +35,7 @@ if __name__ == "__main__":
                     "resnet", {}, 512
                 ),
             },
-            dropout_rate = 0.1,
+            dropout_rate = 0.2,
 
             vector_out_feat = 1024,
             vector_encoder_type = "res_mlp",
@@ -36,18 +43,48 @@ if __name__ == "__main__":
                 hidden_dim = 1024,
                 num_blocks = 2,
                 expansion = 2,
-                dropout_rate = 0.1,
+                dropout_rate = 0.2,
                 is_output_proj = False
             ),
         ),
+
         decoder_type = "res_mlp",
         decoder_kwargs = dict(
             hidden_dim = 2048,
             num_blocks = 2,
             expansion = 2,
-            dropout_rate = 0.1
+            dropout_rate = 0.15,
+            # 接输出头
+            is_output_proj = False,
+            num_out_feats = 2048,
         ),
-        stats_path = "assets/train_stats_depth.json",
+
+        use_multi_head_decoder = True,
+        multi_head_decoder_config = dict(head_decoder_config = dict(
+            joint = (
+                (0, 7),
+                dict(
+                    mlp_layers = [256],
+                    dropout_rate = 0.1
+                )
+            ),
+            torso = (
+                (7, 8),
+                dict(
+                    mlp_layers = [256],
+                    dropout_rate = 0.1
+                )
+            ),
+            base = (
+                (8, 10),
+                dict(
+                    mlp_layers = [256],
+                    dropout_rate = 0.1
+                )
+            )
+        )),
+
+        stats_path = "assets/train_stats_compress_qvel.json",
         data_module = dm,
         loss_type = "smooth_l1",
 
